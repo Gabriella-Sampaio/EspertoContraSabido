40cfc2ecb43ded1186024f04bcb5f21aec7c002c
Gabriella-Sampaio <dixxgabriellasam@gmail.com>
2025-05-23 22:43:02 -0300

    início

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+
+int alimento, 
+nrRepeticaoPicaPau = 0,
+alimentoRaposinha = 0;
+
+Console.Write("Alimentos na mesa: ");
+alimento = Convert.ToInt32(Console.ReadLine());
+
+while (alimento !> 0)
+{
+    nrRepeticaoPicaPau = nrRepeticaoPicaPau + 1;
+    alimento = alimento - nrRepeticaoPicaPau - 1; //ok?
+
+    Console.Write($"Repetição: {nrRepeticaoPicaPau}; Alimento: {alimento} ");
+    Console.ReadKey();
+}
