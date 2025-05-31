863c5e5312db134c9af2da68a77f9dad50a2c180
Gabriella-Sampaio <dixxgabriellasam@gmail.com>
2025-05-31 01:37:41 -0300

    solução lógica, falta corrigir inconsistências

--- Program.cs
+++ Program.cs
@@ -13,41 +13,51 @@ alimento = alimentoDig;
 while (alimento > 0) //enquanto o alimento é maior q 0
 {
     reptPicaPau++; //nm de repetição/ contador && quantidade de alimento do pica-pau 
-    Console.Write($"{reptPicaPau} pra vc. ");
+    Console.Write($"{reptPicaPau} pra vc");
     // Console.ReadKey();
 
-    if (alimento > reptPicaPau)
+    if (resto >= 0) //??? n pode exibir se n tiver nada pra raposinha após (teste nm 28)
     {
-        alimento = alimento - reptPicaPau - 1;
+        Console.Write(", ");
     }
 
-    else // ???
-    {
-        alimento = resto;
-    }
-
-
-
-    //             TESTES
-        //1ª   20    =   22     -      1      - 1
-        //2ª   17    =   20     -      2      - 1
-        //3ª   13    =   17     -      3      - 1
-        //4ª    8    =   13     -      4      - 1
-        //5ª    2    =    8     -      5      - 1
-        //6ª   -5    =    2     -      6      - 1
 
     int colunaRaposinha = 0;
-    while (reptPicaPau > colunaRaposinha )//|| alimento > 0 || resto > 0 ???
+    while (reptPicaPau > colunaRaposinha) //|| resto >0
     {
+        resto = alimento - colunaRaposinha;
+
         colunaRaposinha++; //só aumenta neste while
-        Console.Write($"{colunaRaposinha} "); //só exibe a quantidade de vezes que esse 
-                                              // bloco é executado, quando sai ele volta a ser 0
-        
-        resto = alimento - colunaRaposinha; // ???
-        // Console.ReadKey();
+
+        if (resto - 1 > 0) // SOLUÇÃO!!!!!
+        {
+            Console.Write($"{colunaRaposinha} "); //só exibe a quantidade de vezes que esse 
+                                                  // bloco é executado, quando sai ele volta a ser 0
+            //add contador??? pra questão do último "pra mim"
+        }
+
+        Console.Write($" (resto: {resto}) ");
+        //RESOLVER A QUESTÃO DO TESTE NM 28, A VIRGULA NO NM E 
     }
-    
-    Console.WriteLine($"pra mim. Teste qt alimento - {alimento}");
+
+    // if (alimento > 0) //??? não pode exibir se o while a cima n exibir nada (teste nm 22)
+    // {
+        
+    // }
+
+
+    if (resto > 0)
+    {
+        alimento = alimento - reptPicaPau - 1;
+        Console.WriteLine($"pra mim. ");
+    }
+
+    else
+    {
+        alimento = 0;
+        // Console.WriteLine($"resto: {resto}, alimento {alimento}");
+    }
+
 }
 
 alimRaposinha = alimentoDig - reptPicaPau;
